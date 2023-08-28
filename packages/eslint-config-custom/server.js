45c6caab3b0c207a93530f308ba4e34527419d96
Averyanov Oleg <o.averyanov@qvant.ru>
2023-08-28 13:13:23 +0300

    init

---
+++ packages/eslint-config-custom/server.js
@@ -0,0 +1,39 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+console.log(project);
+
+module.exports = {
+  extends: [
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    "@typescript-eslint/consistent-type-imports": "off"
+
+  },
+};
