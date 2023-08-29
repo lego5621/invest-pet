78a17df90253d868cb4008d6ffc0f133e74f98f3
Averyanov Oleg <o.averyanov@qvant.ru>
2023-08-29 15:04:21 +0300

    fix: sever eslint conf

--- packages/eslint-config-custom/server.js
+++ packages/eslint-config-custom/server.js
@@ -33,7 +33,9 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
-    "@typescript-eslint/consistent-type-imports": "off"
-
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'off',
   },
 };
